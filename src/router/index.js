5c63980d367f1dd73bb9412fa1b1fa41e295f3e2
Francisco Vena <fvena32@gmail.com>
2020-03-11 13:54:26 +0100

    feat(Views): Maquetado el layout base de la documentación

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,39 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+
+// Views
+import DocsLayout from '@/views/Docs.view.vue';
+import NotFoundView from '@/views/NotFound.view.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    redirect: {
+      name: 'section',
+    },
+  },
+
+  // Docs routes
+  {
+    path: '/:section*',
+    name: 'section',
+    component: DocsLayout,
+    pathToRegexpOptions: { strict: false },
+  },
+
+  // Routes not found - 404
+  {
+    path: '/404',
+    name: '404',
+    component: NotFoundView,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '*',
+    redirect: {
+      name: '404',
+    },
   },
 ];
 
---
+++ src/views/Docs.view.vue
@@ -0,0 +1,156 @@
+<template lang="pug">
+.docs
+  //- Header Area
+  .docs__header Cabecera
+
+  .docs__main
+    //- Sidebar Area
+    .docs__sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+      div Sidebar
+
+    //- Content Area
+    .docs__content
+      .docs__wrapper
+        //- Article Area
+        .docs__article
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+          div Artículo
+
+        //- Footer Area
+        .docs__footer Footer
+</template>
