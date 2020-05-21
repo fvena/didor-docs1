d327a22386bdcf67ae47129457f93d1d6088a266
Francisco Vena <fvena32@gmail.com>
2020-05-21 16:24:21 +0200

    revert(demo): elimino el antiguo componente Demo

--- src/components/Demo/Demo.spec.js
+++
@@ -1,8 +0,0 @@
-// import { shallowMount, RouterLinkStub } from '@vue/test-utils';
-// import AppMenu from './AppMenu.vue';
-
-describe('Codegroup', () => {
-  test('prueba', () => {
-    expect(true).toBe(true);
-  });
-});
