48803d21035ded51a143131955c867828f90b581
Francisco Vena <fvena32@gmail.com>
2020-07-10 09:44:59 +0200

    refactor(sidebar): actualizado el componente con un componente hijo

--- src/components/AppSidebar/AppSidebar.vue
+++ src/components/AppSidebar/AppSidebar.vue
@@ -1,26 +1,21 @@
 <template lang="pug">
 .sidebar
-  .sidebar__wrapper
-    //- Level 1
-    nav.sidebar__list.sidebar__list--level1(v-if="links.length")
-      li.sidebar__item(v-for="(level1, key1) in links" :key="key1")
-        router-link(:to="level1.link" v-if="level1.link") {{ level1.title }}
-
-        //- Level 2
-        .sidebar__list.sidebar__list--level2(v-else)
-          .sidebar__title {{ level1.title }}
-          li.sidebar__item(v-for="(level2, key2) in level1.children" :key="key2")
-            router-link(:to="level2.link" v-if="level2.link") {{ level2.title }}
-
-            //- Level 3
-            .sidebar__list.sidebar__list--level3(v-else)
-              .sidebar__title {{ level2.title }}
-              li.sidebar__item(v-for="(level3, key3) in level2.children" :key="key3")
-                router-link(:to="level3.link") {{ level3.title }}
+  nav.sidebar__wrapper(v-if="links.length")
+    AppSidebarItem(
+      v-for="(link,key) in links"
+      :key="key"
+      :link="link"
+      :deep="1")
 </template>
 
 <script>
+import AppSidebarItem from './AppSidebarItem.vue';
+
 export default {
+  name: 'AppSidebar',
+  components: {
+    AppSidebarItem,
+  },
   props: {
     links: {
       type: Array,
